e81424b3ab1245d534ac9bbb021a2ff582c02333
Jinhui Zhen <123280886+Jinzeld@users.noreply.github.com>
2025-02-04 14:28:01 -0800

    Added colors to the skills icon logos

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,41 +1,40 @@
 import React from "react";
-import { 
-  FaHtml5, FaCss3Alt, FaPhp, FaJs, FaDatabase, FaPython, FaJava, FaReact, FaNodeJs, FaGitAlt 
-} from "react-icons/fa";
-import { SiCplusplus, SiLatex, SiEmberdotjs, SiWordpress, SiDocker, SiFlutter} from "react-icons/si"; 
-import { MdLanguage } from "react-icons/md";
+
+import {SiEmberdotjs} from "react-icons/si"; 
 import "./skills.css";
 
 const Skills = () => {
   // Skills with icons
   const skills = {
     languages: [
-      { name: "Python", icon: <FaPython /> },
-      { name: "C/C++", icon: <SiCplusplus /> },
-      { name: "HTML", icon: <FaHtml5 /> },
-      { name: "CSS", icon: <FaCss3Alt  /> },
-      { name: "JavaScript", icon: <FaJs /> },
-      { name: "SQL", icon: <FaDatabase /> },
-      { name: "Java", icon: <FaJava /> },
-      { name: "PHP", icon: <FaPhp  /> },
-      { name: "LaTeX", icon: <SiLatex /> },
-    ],
+      { name: "Python", icon: <img src="https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg" alt="Python" width="24" /> },
+      { name: "C/C++", icon: <img src="https://upload.wikimedia.org/wikipedia/commons/1/18/C_Programming_Language.svg" alt="C++" width="24" /> },
+      { name: "HTML", icon: <img src="https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg" alt="HTML" width="24" /> },
+      { name: "CSS", icon: <img src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg" alt="CSS" width="24" /> },
+      { name: "JavaScript", icon: <img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" alt="JavaScript" width="24" /> },
+      { name: "SQL", icon: <img src="https://upload.wikimedia.org/wikipedia/commons/8/87/Sql_data_base_with_logo.png" alt="SQL" width="24" /> },
+      { name: "Java", icon: <img src="https://upload.wikimedia.org/wikipedia/en/3/30/Java_programming_language_logo.svg" alt="Java" width="24" /> },
+      { name: "PHP", icon: <img src="https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg" alt="PHP" width="24" /> },
+      { name: "LaTeX", icon: <img src="https://upload.wikimedia.org/wikipedia/commons/9/92/LaTeX_logo.svg" alt="LaTeX" width="24" /> },
+    ],  
     frameworks: [
-      { name: "React", icon: <FaReact /> },
-      { name: "Node.js", icon: <FaNodeJs /> },
-      { name: "Ember.js", icon: <SiEmberdotjs /> },
-      { name: "Flutter", icon: <SiFlutter /> }, 
-      { name: "WordPress", icon: <SiWordpress /> },
+      { name: "React", icon: <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" alt="React" width="24" /> },
+      { name: "Node.js", icon: <img src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg" alt="Node.js" width="24" /> },
+      { name: "Ember.js", icon: <SiEmberdotjs style={{ color: "#E04E39" }}/> },
+      { name: "WordPress", icon: <img src="https://upload.wikimedia.org/wikipedia/commons/9/98/WordPress_blue_logo.svg" alt="WordPress" width="24" /> },
+      { name: "Selenium", icon: <img src="https://upload.wikimedia.org/wikipedia/commons/d/d5/Selenium_Logo.png" alt="Selenium" width="24" /> },
+      { name: "Playwright", icon: <img src="https://playwright.dev/img/playwright-logo.svg" alt="Playwright" width="24" /> },
     ],
     developerTools: [
-      { name: "Git", icon: <FaGitAlt /> },
-      { name: "Docker", icon: <SiDocker /> },
+      { name: "Git", icon: <img src="https://upload.wikimedia.org/wikipedia/commons/3/3f/Git_icon.svg" alt="Git" width="24" /> },
+      { name: "Docker", icon: <img src="https://upload.wikimedia.org/wikipedia/commons/4/4e/Docker_%28container_engine%29_logo.svg" alt="Docker" width="24" /> },
+      { name: "Visual Studio Code", icon: <img src="https://upload.wikimedia.org/wikipedia/commons/9/9a/Visual_Studio_Code_1.35_icon.svg" alt="VS Code" width="24" /> },
      
     ],
     languagesSpoken: [
-      { name: "English", icon: <MdLanguage /> },
-      { name: "Chinese (Mandarin)", icon: <MdLanguage /> },
-      { name: "Chinese (Cantonese)", icon: <MdLanguage /> },
+      { name: "English", icon: <img src="https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg" alt="English" width="24" /> },
+      { name: "Chinese (Mandarin)", icon: <img src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Flag_of_the_People%27s_Republic_of_China.svg" alt="Mandarin" width="24" /> },
+      { name: "Chinese (Cantonese)", icon: <img src="https://upload.wikimedia.org/wikipedia/commons/5/5b/Flag_of_Hong_Kong.svg" alt="Cantonese" width="24" /> },
     ],
   };
 
